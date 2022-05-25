9c0b68b6499f438f8ca3c1f4d630bc04023226d4
Andy Zeng <andy@Andys-MacBook-Pro.local>
2022-05-25 17:21:55 +1000

    export env

--- train.data_processing.sh
+++ train.data_processing.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ENVLOC="${SCRIPT_DIR}/.env"
+if [ -f $ENVLOC ]
+then
+    export $(cat $ENVLOC | xargs)
+fi
+
 BASEDIR="$(dirname "$SCRIPT_DIR")"
 
 PYLOC="${BASEDIR}/anaconda3/envs/brewcore/bin/python"
